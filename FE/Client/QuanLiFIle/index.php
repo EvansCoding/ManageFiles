ee1dfd370a3282eba74201636361a383fa8b8496
EvansCoding <evans.coding228@gmail.com>
2020-04-09 23:28:12 +0700

    Add: Page admin include: page[files, links, folders]

---
+++ FE/Client/QuanLiFIle/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>2K | Free Document | Login</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header-logo">
+            <img src="./img/mylogo.png" alt="logo" class="header-logo__img">
+        </div>
+        <div class="header-login">
+            <form action="#" method="GET" class="header__form">
+                <div class="block">
+                    <i class="fas fa-user"></i>
+                    <input type="text" class="header__form--id header__form--text" placeholder=" User name">
+                </div>
+                <div class="block">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" class="header__form--password header__form--text" placeholder=" Password">
+                </div>
+                <div class="block">
+                    <a href="#" class="header__form--forgot">For got password?</a>
+                    <input type="submit" class="btn" value="Login">
+                </div>
+                <div class="clear"></div>
+            </form>
+        </div>
+    </header>
+</body>
+<script defer src="https://use.fontawesome.com/releases/v5.1.1/js/all.js" integrity="sha384-BtvRZcyfv4r0x/phJt9Y9HhnN5ur1Z+kZbKVgzVBAlQZX4jvAuImlIz+bG7TS00a" crossorigin="anonymous"></script>
+
+</html>
